1880d36644ad38cf1f554edcd1c9436d1ace3729
kimhyeok <kimmang940715@naver.com>
2023-10-19 04:06:51 +0900

    배열 중반 추가

--- Data/Property/Program.cs
+++ Data/Property/Program.cs
@@ -8,6 +8,138 @@ namespace Property
 {
     internal class Program
     {
+        /*
+         * 프로퍼티
+         * 자동구현 프로퍼티
+         * 프로퍼티를 이용한 객체 초기화
+         * 무명 형식
+         * 
+         * public 필드는 외부 객체에 의해 오염될 가능성을 열어두는것
+         * 자바같은 언어에서는 Get/Set 메소드를 이용하여 내부 필드에 접근하도록 함
+         * C# 에서는 Get/Set 메소드 뿐 아니라, 필드만큼 편리한 프로퍼티를 제공
+         * 
+         * class MyClass
+         * {
+         *      private int myField;
+         *      public int GetMyFierId() { return myField; }
+         *      public void SetMyField ( int NewValue ) { myField = NewValue; } ;
+         *  }
+         *  
+         *  MyClass obj = new MyClass();
+         *  obj.SetMyField(3);
+         *  Console.WriteLine(obj.GetMyField());
+         *  
+         *  
+         *  프로퍼티란?
+         *   public 필드를 다루듯 내부 필드에 접근하게 해주는 멤버
+         *   외부에 데이터를 출력 할 때는 get접근자
+         *   내부에 데이터를 입력 할 때는 set접근자
+         *   
+         *   class 클래스이름
+         *   {
+         *      데이터형식 필드이름;
+         *      접근한정자 데이터형식 프로퍼티이름
+         *      {
+         *          get
+         *          {
+         *              return 필드이름;
+         *          }
+         *          set
+         *          {
+         *              필드이름 = value;
+         *          }
+         *       }
+         *    }
+         *    
+         *    ex :
+         *    class MyClass
+         *    {
+         *      private int myField;
+         *      public int MyField
+         *      {
+         *          get
+         *          {
+         *              return myField;
+         *          }
+         *          set
+         *          {
+         *              myField = value;
+         *          }
+         *      }
+         *    }
+         *    
+         *    MyClass obj = new MyClass();
+         *    obj.MyField = 3;
+         *    Console.WriteLine(obj.MyField);
+         *         
+         *         
+         *    자동구현 프로퍼티
+         *     프로퍼티 구현시, 뻔하게 작성되는 코드를 생략할 수 있음
+         *     필드 이름 생략
+         *     get 접근자의 반환문(return 필드;) 생략
+         *     set 접근자의 필드 수정 (필드 = value;) 코드 생략
+         *     
+         *     public class NameCard
+         *     {
+         *          private string name;
+         *          private string phoneNumber;
+         *          
+         *          public string Name
+         *          {
+         *              get { return name; }
+         *              set { name = value; }
+         *          }
+         *          
+         *          public string PhoneNumber
+         *          {
+         *              get { return phoneNumber; }
+         *              set { phoneNumber = value; }
+         *          }
+         *      }
+         *      
+         *      VS 
+         *      
+         *      public class NameCard
+         *      {
+         *          public string Name
+         *          {
+         *              get; set;
+         *          }
+         *          public string PhoneNumber
+         *          {
+         *              get; set;
+         *          }
+         *      }
+         *      
+         *      
+         *      프로퍼티를 이용한 객체 초기화
+         *          객체를 생성할 때 프로퍼티를 이용한 초기화 가능
+         *          프로퍼티를 이용한 초기화는 다음과 같은 형식으로 함
+         *          
+         *          클래스 이름 인스턴스 = new 클래스이름()
+         *          {
+         *              프로퍼티1 = 값,
+         *              프로퍼티2 = 값,
+         *              프로퍼티3 = 값,
+         *          }
+         *          
+         *          프로퍼티를 이용한 초기화의 예
+         *          BirthdayInfo birth = new BirthdayInfo()
+         *          {
+         *              Name = "고윤정",
+         *              Birthdat = new DateTime(1996,6,28);
+         *          }
+         *          
+         *      무명 형식
+         *      public 프로퍼티로만 이루어진 이름없는 클래스
+         *      1회용 데이터형식 으로 사용
+         *      무명 형식은 var 키워드와 프로퍼티를 이용하여 선언함
+         *      무명 형식의 선언예와 사용 예는 다음과 같음
+         *      
+         *      var myInstance = new { Name = "고윤정", Age = "28" };
+         *      Console.WriteLine (myInstance.Name, myInstance.Age);
+         * 
+         */
         static void Main(string[] args)
         {
         }
---
+++ Data/_07_Array/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_Array
+{
+    /*
+     * 
+     * 배열
+     * 배열 초기화 방법
+     * System.Array 클래스
+     * 다차원 배열
+     * 컬렉션
+     * 인덱서
+     * 
+     * 배열 (Array)
+     *  같은 형식의 복수 인스턴스를 저장 할 수 있는 형식
+     *  참조형식 으로써 연속된 메모리 공간을 가리킴
+     *  반복문, 특히 for, foreach 문과 함께 사용하면 효율 향상
+     *  
+     *  배열 선언
+     *  데이터형식[] 배열이름 = new 데이터형식[];
+     *  ex:
+     *  int[] array = new int[5];
+     *  
+     *  배열을 초기화하는 방법 3가지
+     *  1. 기본형
+     *      배열의 원소 개수를 명시
+     *      배열의 각 원소에 입력될 데이터를 입력 { } 로 감싼 블록을 붙힘
+     *      string[] arr1 = new string[3] {"1", "2", "3"};
+     *  2. 요소개수 생략
+     *      배열의 요소 개수 생략
+     *      배열의 각 원소에 입력될 데이터를 입력 { } 로 감싼 블록을 붙임
+     *      string[] arr2 = new string[] {"4", "5", "6"};
+     *  3. new 연산자 형식, 요소 개수 생략
+     *      new 연산자, 형식과 괄호 [ ] , 배열의 용량을 모두 생략
+     *      배열의 각 원소에 입력될 데이터를 입력 { } 로 감싼 블록을 붙임
+     *      string[] arr3 = {"7", "8", "9"};
+     *  
+     *  System.Array 클래스
+     *      모든 배열의 기반 클래스 이며 배열 그 자체를 나타냄
+     *      배열을 보다 편리하게 다룰 수 있게 도와주는 유틸리티 메소드 제공
+     *      정렬, 탐색, 크기 조정 등의 기능 제공
+     *      
+     *  다차원 배열
+     *      2개의 차원으로 요소 배치
+     *          차원의 길이를 뒤에서부터 읽어 해석
+     *              ex : int[2,3] : 길이가 3인 1차원 배열을 요소로 갖는 길이가 2인 2차원 배열
+     *              
+     *  가변 배열
+     *      배열의 배열, 즉 배열을 요소로 갖는 배열
+     *      요소로 입력되는 배열의 차우너과 길이는 같아야 할 필요없음
+     *      선언 형식
+     *      데이터형식[] [] 배열의 이름 = enw 데이터형식 [][];
+     *      ex:
+     *      int [][] jagged = new int[3][];
+     *      jagged[0] = new int [5] {1, 2, 3, 4, 5};
+     *      jagged[1] = new int [] {10, 20, 30};
+     *      jagged[2] = new int [] {100, 200};
+     *      
+     *      int[][] jagged2 = new int[2][] { new int[] { 1000, 2000}, new int[4] {6, 7, 8, 9} };
+     *      
+     *  컬렉션(Collection)
+     *      같은 성격을 띄는 데이터의 모음을 다루는 자료 구조
+     *      배열(System.Array)도 .NET이 제공하는 컬렉션 자료구조 중 하나
+     *      ArrayList
+     *      Queue
+     *      Stack
+     *      HashTable
+     *      
+     *  ArrayList
+     *      배열과 같이 인덱스를 이용하여 요소에 접근가능
+     *      필요에 따라 동적으로 크기가 증가
+     *      요소를 추가하는 Add(), 삭제하는 RemoveAt(), 삽입하는 Insert()메소드 등을 제공
+     *      ArrayList list = new ArrayList();
+     *      list.Add(10);
+     *      list.Add(20);
+     *      list.Add(30);
+     *      
+     *      list.RemoveAt(1);
+     *      list.Insert(25, 1);
+     *      list[0] = 11;
+     *      Console.WriteLine(list[0]);
+     *      
+     *  Queue
+     *      선입선출(FIFO : First In First Out) 구조의 자료구조
+     *      데이터를 입력하는 Enqueue(), 출력하는 Dequeue() 메소드 제공
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
